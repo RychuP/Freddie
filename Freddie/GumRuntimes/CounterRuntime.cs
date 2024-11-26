3ff35a6d23597b445950250c2a1e22aa9c0c9117
Ryszard Pyka <60892050+RychuP@users.noreply.github.com>
2024-11-26 19:12:40 +0000

    Added game hud.

---
+++ Freddie/GumRuntimes/CounterRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Freddie.GumRuntimes
+{
+    public partial class CounterRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
